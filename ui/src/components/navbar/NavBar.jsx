7da57820a7cf06048ee0077bebfeb35e39bd0b50
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-04-26 10:26:19 +0200

    Composant navbar

--- ui/src/components/navbar/NavBar.jsx
+++ ui/src/components/navbar/NavBar.jsx
@@ -1,21 +1,28 @@
 /* import { Link, useLocation } from 'react-router-dom'; */
 import logo from '../../assets/argentBankLogo.png';
 import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
+
 
 function NavBar() {
 
   return (
-    <nav class="main-nav">
-      <a class="main-nav-logo" href="./index.html">
-        <img class="main-nav-logo-image" src={logo} alt="Argent Bank Logo"/>
-        <h1 class="sr-only">Argent Bank</h1>
-      </a>
-      <div>
-        <a class="main-nav-item" href="./sign-in.html">
-          <i class="fa fa-user-circle"></i>
-          Sign In
-        </a>
-      </div>
+    <nav className="main-nav">
+      <ul>
+        <li>
+          <a className="main-nav-logo" href="/">
+            <img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo"/>
+            <h1 className="sr-only">Argent Bank</h1>
+          </a>
+      </li>
+      <li>
+          <a className="main-nav-item" href="./sign-in.html">
+          <FontAwesomeIcon className="singup-icon" icon={faCircleUser}/>
+            Sign In
+          </a>
+      </li>
+      </ul>
     </nav>
   )
 }

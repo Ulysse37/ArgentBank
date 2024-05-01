91a92c1f27b15edc1f70c04d7dd3cfe75fa139ee
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-05-01 09:06:01 +0200

    Fix composant feature

--- ui/src/pages/home/Home.jsx
+++ ui/src/pages/home/Home.jsx
@@ -4,8 +4,7 @@ import ChatIcon from '../../assets/icon-chat.png';
 import MoneyIcon from '../../assets/icon-money.png';
 import SecurityIcon from '../../assets/icon-security.png';
 
-/* import Feature from '../../components/feature/Feature.Jsx'; */
-
+import Feature from '../../components/feature/Feature';
 
 function Home() {
 
@@ -22,38 +21,18 @@ function Home() {
       </div>
       <section className="features">
         <h2 className="sr-only">Features</h2>
-        {/* <Feature /> */}  {/* //! String contains an invalid character  */}
-        <div className="feature-item">
-          <img src={ChatIcon} alt="Chat Icon" className="feature-icon" />
-          <h3 className="feature-item-title">You are our #1 priority</h3>
-          <p>
-            Need to talk to a representative? You can get in touch through our
-            24/7 chat or through a phone call in less than 5 minutes.
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src={MoneyIcon}
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">More savings means higher rates</h3>
-          <p>
-            The more you save with us, the higher your interest rate will be!
-          </p>
-        </div>
-        <div className="feature-item">
-          <img
-            src={SecurityIcon}
-            alt="Chat Icon"
-            className="feature-icon"
-          />
-          <h3 className="feature-item-title">Security you can trust</h3>
-          <p>
-            We use top of the line encryption to make sure your data and money
-            is always safe.
-          </p>
-        </div>
+        <Feature 
+          imgSrc={ChatIcon} imgAlt="Icône de communication" 
+          title="You are our #1 priority" 
+          text="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />  
+        <Feature 
+          imgSrc={MoneyIcon} imgAlt="Icône d'argent'" 
+          title="More savings means higher rates" 
+          text="The more you save with us, the higher your interest rate will be!" />  
+        <Feature 
+          imgSrc={SecurityIcon} imgAlt="Icône de sécurité'" 
+          title="Security you can trust" 
+          text="We use top of the line encryption to make sure your data and money is always safe." /> 
       </section>
     </main>
   );

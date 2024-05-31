29483bb83507a0e291999a68990223611f3bcdc9
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-05-31 11:24:11 +0200

    ajout redux/persist + fichiers redux

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'
+import { store } from './redux/store';
 import './index.css';
-/* import App from './App'; */
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/navbar/NavBar.jsx';
 import Home from './pages/home/Home.jsx';
@@ -13,19 +13,16 @@ import Footer from './components/footer/Footer.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/profile" element={<Profile />}></Route>
-      </Routes>
-      <Footer />
-    </Router>
+    {/* <Provider store={store}> */}
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
+    {/* </Provider> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

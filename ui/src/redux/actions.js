1e100faa4de572c00da6e0edfcbb412724c4c033
Ulysse37 <ulysse.courtet@hotmail.fr>
2024-07-12 09:59:20 +0200

    ajout action reseterror dans page login  pour suppr msg erreur connexion

--- ui/src/redux/actions.js
+++ ui/src/redux/actions.js
@@ -1,4 +1,4 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createAction  } from '@reduxjs/toolkit';
 
 export const logIn = createAsyncThunk(
   'auth/logIn',
@@ -74,3 +74,5 @@ export const getProfileInfo = createAsyncThunk(
     }
   }
 );
+
+export const resetError = createAction('auth/resetError'); // action pour remettre le state de error à null
--- ui/src/redux/reducers.js
+++ ui/src/redux/reducers.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { logIn, logOut, getProfileInfo } from '../redux/actions';
+import { logIn, logOut, getProfileInfo, resetError } from '../redux/actions';
 
 const initialState = {
   token: null,
@@ -37,7 +37,11 @@ const authSlice =  createSlice({
       })
       .addCase(getProfileInfo.rejected, (state, action) => {
         state.error = action.payload;
-      });
+      })
+      .addCase(resetError, (state) => {
+        state.error = null; // quand action déclenchée -> met à jour le state de error à null
+      });;
+      
   },
 });
 
